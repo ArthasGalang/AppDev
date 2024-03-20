d6cd026600c40b3032d8b1aa7d0875aa44e3de8e
ArthasGalang <164136853+ArthasGalang@users.noreply.github.com>
2024-03-20 16:58:00 +0800

    main

---
+++ main/capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'main',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
