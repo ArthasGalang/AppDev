0345abd9ff2d722d5ee471d166683e9222c02fe7
ArthasGalang <164136853+ArthasGalang@users.noreply.github.com>
2024-03-20 17:04:58 +0800

    Act1

--- ACTIVITY-1-DueDit-Galang-Galapia-Vargas/capacitor.config.ts
+++ ACTIVITY-1-DueDit-Galang-Galapia-Vargas/capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'main',
+  appName: 'assignment1',
   webDir: 'www',
   server: {
     androidScheme: 'https'
---
+++ ACTIVITY-1-DueDit-Galang-Galapia-Vargas/src/app/assignment1/assignment1-page.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  templateUrl: 'assignment1.html',
+})
+export class Assignment1Page {
+    groupName = "DueDit";
+  constructor() {}
+
+}
